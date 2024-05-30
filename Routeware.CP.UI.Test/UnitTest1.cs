5abdfabca1a23af8db4bd3cb6a3a28bf7f41fc90
Julio Ynojosa <julio.ynojosa@routeware.com>
2024-05-30 14:37:36 -0500

    Add project files.

---
+++ Routeware.CP.UI.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Routeware.CP.UI.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
